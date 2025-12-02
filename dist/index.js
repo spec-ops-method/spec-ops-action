9589b84831a64dccedf717b7001a9677f468eb76
mheadd <mheadd@gmail.com>
2025-12-02 14:59:01 -0500

    Fix CI: simplify dist/ output to only essential files
    - Remove source maps and declaration files from build
    - Only commit index.js and licenses.txt to reduce inconsistencies
    - Prevents CI failures from environment-specific build artifacts

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 4914:
@@ -45263,4 +45263,3 @@ module.exports = parseParams
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
